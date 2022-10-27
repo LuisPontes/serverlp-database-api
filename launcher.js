e4780993f169047a793ac42a144355dde3e69db9
Luis Pontes <Luismarquespontes@gmail.com>
2022-10-27 15:02:16 +0200

    port change to 8080

--- launcher.js
+++ launcher.js
@@ -58,7 +58,7 @@ require("./src/PetSittingApp/routes/PetSittingApp.routes.js")(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });

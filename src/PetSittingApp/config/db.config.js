25b525989b1b1d2523a9fa2a7a1d2a3136b3fc92
Luis Pontes <Luismarquespontes@gmail.com>
2022-10-27 16:28:04 +0200

    db online

--- src/PetSittingApp/config/db.config.js
+++ src/PetSittingApp/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "admin",
-  PASSWORD: "adminadmin",
-  DB: "testdb",
+  HOST: "bx7dvqsjek21kznwngrt-mysql.services.clever-cloud.com",
+  USER: "uzzz27v0wzs3jyhh",
+  PASSWORD: "f8EPG5ExjqZt2RLcPdV2",
+  DB: "bx7dvqsjek21kznwngrt",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +11,16 @@ module.exports = {
     idle: 10000
   }
 };
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "admin",
+//   PASSWORD: "adminadmin",
+//   DB: "testdb",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
